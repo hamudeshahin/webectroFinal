acfed66b306d56dee029fdbd36c020be24e590cd
hamudeshahin <hamedsahin2018@gmail.com>
2022-01-19 21:09:04 +0300

    removed 404 and 500 page

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-  return (
-    <div>
-      <h2>Not Found !</h2>
-    </div>
-  );
-}
-
-export default NotFound;
--- pages/500.js
+++
@@ -1,9 +0,0 @@
-function Error() {
-  return (
-    <div>
-      <h2>500 Error !</h2>
-    </div>
-  );
-}
-
-export default Error;
